6f5313e80b9ae4ed120e71ca74d99d4e8e8ba55e
Corrado <corrado.lanera@gmail.com>
2022-03-23 01:14:44 +0100

    import_trd

--- weaning-imports.R
+++ weaning-imports.R
@@ -1,3 +1,6 @@
+library(testthat)
+library(checkmate)
+
 library(tidyverse)
 library(lubridate)
 library(here)
@@ -5,7 +8,30 @@ library(here)
 
 # Functions -------------------------------------------------------
 
+import_trd <- function(path) {
+  stopifnot(stringr::str_detect(path, "TRD"))
+  assert_file_exists(path)
 
+  # for future development
+  headr <- readr::read_lines(path, n_max = 20)
+
+  content <- read_lines(path, skip = 20) |>
+    str_subset("Riassunto", negate = TRUE)
+
+  I(content) |>
+    readr::read_tsv(col_names = FALSE, col_types = "c") |>
+    unheadr::mash_colnames(
+      n_name_rows = 2,
+      keep_names = FALSE,
+      sliding_headers = TRUE
+    ) |>
+    janitor::remove_empty("cols") |>
+    janitor::clean_names() |>
+    dplyr::filter(if_any(-.data[["ora"]], ~!is.na(.x))) |>
+    dplyr::mutate(
+      dplyr::across(dplyr::everything(), readr::parse_guess)
+    )
+}
 
 
 
@@ -14,6 +40,9 @@ library(here)
 
 # Executions/Experiments ------------------------------------------
 
+here::here("data-raw/AB/AB123_8_TRD.SI") |>
+  import_trd() |>
+  dplyr::glimpse()
 
 
 
@@ -23,24 +52,39 @@ library(here)
 
 # tests -----------------------------------------------------------
 
-library(testthat)
-library(checkmate)
-
-with_reporter(default_reporter(), {
-# with_reporter(check_reporter(), {
+with_reporter(
+  # default_reporter(), {
+  check_reporter(), {
 
 
-  context(" ")
+    context(" ")
 
-  test_that("import_trd works", {
-    # setup
+    test_that("import_trd works", {
+      # setup
+      sample_path <- here::here("data-raw/AB/AB123_8_TRD.SI")
+      fake_trd_path <- fs::file_temp(pattern = "TRD", ext = "SI")
+      fake_path <- fs::file_temp(ext = "SI")
 
-    # evaluation
+      # evaluation
+      res <- import_trd(sample_path)
 
-    # tests
+      # tests
+      res |>
+        expect_tibble(
+          ncols = 21,
+          types = c("hms", rep("numeric", 20)),
+          min.rows = 1,
+          all.missing = FALSE
+        )
+
+      import_trd(fake_trd_path) |>
+        expect_error("File does not exist")
+
+      import_trd(fake_path) |>
+        expect_error("TRD")
+
+    })
 
 
-  })
-
-
-})
+  }
+)
