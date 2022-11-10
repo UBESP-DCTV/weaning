f46f63cf84a1fc474d2f64711f2d9c32cf657008
andredot <andrea.pedot@ubep.unipd.it>
2022-11-10 09:29:47 +0100

    updated plot_trd function to match existing pipeline

--- R/plot_trd.R
+++ R/plot_trd.R
@@ -55,26 +55,26 @@ plot_trd_vars <- function(weaning_subset = NA) {
                            min.rows = 1)
 
   trd_subset <- tar_read(weaningsTRD) %>%
-    mutate( id_univoco = ifelse(
+    dplyr::mutate( id_univoco = ifelse(
       test = id_pat <10,
       yes = paste0(folder, "00", id_pat),
       no = paste0(folder, "0", id_pat) ) ) %>%
-    filter( id_univoco %in% weaning_subset[["id_univoco"]],
-            date %in% weaning_subset[["data_lettura"]])
+    dplyr::filter( id_univoco %in% weaning_subset[["id_univoco"]],
+                   date %in% weaning_subset[["data_lettura"]]) %>%
+    dplyr::select(id_univoco,
+                  date,
+                  ora,
+                  lavoro_respiratorio_del_ventilatore_joule_l,
+                  lavoro_respiratorio_del_paziente_joule_l,
+                  pressione_di_fine_esp_cm_h2o,
+                  press_media_vie_aeree_cm_h2o) %>%
+    tidyr::pivot_longer(cols = 4:7)
 
   plot <- ggplot2::ggplot(trd_subset,
-                          aes( x = ora)) +
-    ggplot2::geom_step(aes(
-      y = lavoro_respiratorio_del_ventilatore_joule_l *10) ) +
-    ggplot2::geom_step(aes(
-      y = lavoro_respiratorio_del_paziente_joule_l *10),
-      color = "dark blue") +
-    ggplot2::geom_step(aes(
-      y = pressione_di_fine_esp_cm_h2o),
-      color = "dark green") +
-    ggplot2::geom_step(aes(
-      y = press_media_vie_aeree_cm_h2o),
-      color = "dark orange") +
+                          aes( x = ora,
+                               y = value,
+                               color = name)) +
+    ggplot2::geom_step() +
     ggplot2::labs(title = "Weanings TRD",
                   x = "", y = "") +
     ggplot2::facet_wrap(vars(id_univoco, date))
@@ -88,24 +88,26 @@ plot_trd_files <- function(weaning_subset = NA) {
                            min.rows = 1)
 
   trd_subset <- tar_read(weaningsTRD) %>%
-    mutate( id_univoco = ifelse(
+    dplyr::mutate( id_univoco = ifelse(
       test = id_pat <10,
       yes = paste0(folder, "00", id_pat),
       no = paste0(folder, "0", id_pat) ) ) %>%
-    filter( id_univoco %in% weaning_subset[["id_univoco"]],
-            date %in% weaning_subset[["data_lettura"]])
+    dplyr::filter( id_univoco %in% weaning_subset[["id_univoco"]],
+                   date %in% weaning_subset[["data_lettura"]]) %>%
+    dplyr::select(id_univoco,
+                  date,
+                  ora,
+                  lavoro_respiratorio_del_ventilatore_joule_l,
+                  lavoro_respiratorio_del_paziente_joule_l,
+                  pressione_di_fine_esp_cm_h2o,
+                  press_media_vie_aeree_cm_h2o) %>%
+    tidyr::pivot_longer(cols = 4:7)
 
   plot <- ggplot2::ggplot(trd_subset,
-                          aes( x = ora,
-                               color = file)) +
-    ggplot2::geom_step(aes(
-      y = lavoro_respiratorio_del_ventilatore_joule_l *10) ) +
-    ggplot2::geom_step(aes(
-      y = lavoro_respiratorio_del_paziente_joule_l *10)) +
-    ggplot2::geom_step(aes(
-      y = pressione_di_fine_esp_cm_h2o)) +
-    ggplot2::geom_step(aes(
-      y = press_media_vie_aeree_cm_h2o)) +
+                          aes(  x = ora,
+                                y = value,
+                                color = name)) +
+    ggplot2::geom_step() +
     ggplot2::labs(title = "Weanings TRD",
                   x = "", y = "") +
     ggplot2::facet_wrap(vars(id_univoco, date)) +
