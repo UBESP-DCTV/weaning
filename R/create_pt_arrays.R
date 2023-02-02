83c7b24f86ca6b9af86959707a4127b5f77dd660
Corrado Lanera <Corrado.Lanera@gmail.com>
2023-02-02 10:41:46 +0100

    removed create_pt_outcome (don't need it anymore)

--- R/create_pt_arrays.R
+++ R/create_pt_arrays.R
@@ -30,9 +30,9 @@ create_pt_ptnames <- function(db, pt_id) {
       )
     ) |>
     dplyr::mutate(
-      type = factor(type, levels = c("nava", "psv")),
-      sesso = factor(sesso, levels = c("F", "M")),
-      reason = factor(reason, levels = reason_levels)
+      type = factor(.data[["type"]], levels = c("nava", "psv")),
+      sesso = factor(.data[["sesso"]], levels = c("F", "M")),
+      reason = factor(.data[["reason"]], levels = reason_levels)
     ) |>
     unlist(use.names = FALSE) |>
     array()
@@ -58,7 +58,7 @@ create_pt_weanings <- function(db, pt_id) {
 
 add_sbt <- function(db_reg) {
   db_reg |>
-    dplyr::group_by(id_univoco) |>
+    dplyr::group_by(.data[["id_univoco"]]) |>
     dplyr::arrange(.data[["data_lettura"]], .by_group = TRUE) |>
     dplyr::mutate(
       sbt = dplyr::case_when(
@@ -80,59 +80,9 @@ add_sbt <- function(db_reg) {
 
 
 
-
-
-#' Create outcome array
-#'
-#' from the `db` weaning_days, it extracts (i.e., filter and select)
-#' information on the SBT day and its result for the patient indicated
-#' in `pt_id`
-#'
-#' @param db (data.frame) the weaning_days data
-#' @param pt_id (character(1)) patient's id_univoco to consider
-#'
-#' @return (named list of 1D arrays) with the outcomes for the
-#'  corresponding SBT days
-#' @export
-create_pt_outcome <- function(
-    db,
-    pt_id,
-    esiti = c("Insuccesso", "Successo")
-) {
-
-  # stop("!!!!!!!!!check me!!!!!!!!!!!")
-
-  check_array_creation_inputs(db, pt_id)
-  checkmate::assert_subset("esito", choices = names(db))
-
-  aux <- db |>
-    dplyr::filter(.data[["id_univoco"]] == pt_id) |>
-    dplyr::select(dplyr::all_of(c("giorno_studio", "esito"))) |>
-    dplyr::mutate(
-      esito = as.integer(factor(.data[["esito"]], levels = esiti)) - 1L
-    ) |>
-    dplyr::arrange(.data[["giorno_studio"]])
-
-  aux[["giorno_studio"]] |>
-    purrr::set_names() |>
-    purrr::map(\(day) {
-      dplyr::filter(aux, .data[["giorno_studio"]] == day) |>
-        dplyr::pull() |>
-        array()
-    })
-
-}
-
 check_array_creation_inputs <- function(db, pt_id) {
   checkmate::assert_data_frame(db)
   checkmate::assert_subset("id_univoco", choices = names(db))
   checkmate::assert_string(pt_id)
   checkmate::assert_subset(pt_id, choices = db[["id_univoco"]])
 }
-
-
-
-
-
-
-
