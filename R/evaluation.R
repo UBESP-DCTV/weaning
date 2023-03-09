867479e32fe59e1de57bae0da110190e49569de9
andredot <andrea.pedot@ubep.unipd.it>
2023-03-09 19:31:04 +0100

    complete metrics table with xgboost also
    TODO: custom metric in Evaluation.R

--- R/evaluation.R
+++ R/evaluation.R
@@ -1,37 +1,48 @@
-#predicted <- pred_obs["pred0"]
-#observed <- pred_obs["observed"]
 
-metric_table <- function(df) {
-  #Step 0 : define metrics
-  multi_metric <- yardstick::metric_set(
-    accuracy,
-    bal_accuracy,
-    precision,
-    mcc,
-    recall
+mv_cost <- tibble::tribble(
+  # Thesis classes (0skip / 1fail / 2pass)
+  #   Truth:        0     1     2
+  # Predicted: 0   +1    +1    -1
+  #            1   NA    +1    -1
+  #            2   NA    -1    +1
+  #
+  # Code classes (0skip / 1pass / 2fail)
+  #   Truth:        0     2     1
+  # Predicted: 0   +1    +1    -1
+  #            2   NA    +1    -1
+  #            1   NA    -1    +1
+  #
+  ~truth,   ~estimate, ~cost,
+  "0", "0",  +1,
+  "0", "2",  NA,
+  "0", "1",  NA,
+  "2", "0",  +1,
+  "2", "2",  +1,
+  "2", "1",  -1,
+  "1", "0",  -1,
+  "1", "2",  -1,
+  "1", "1",  +1
+)
+
+# metrics
+clap <- function(data, truth, estimate, na_rm = TRUE, ...) {
+
+  yardstick::classification_cost(
+    data = data,
+    truth = !! rlang::enquo(truth),
+    estimate = !! rlang::enquo(estimate),
+    costs = mv_cost,
+    na_rm = na_rm,
+    case_weights = NULL
   )
-
-  #Step 1: metrics
-    df <- df |>
-    dplyr::mutate(
-      dplyr::across(
-        dplyr::everything(),
-        ~ factor(.x, levels = c(0,1,2))
-  ))
-
-  metrics <- df |>
-    multi_metric(
-      truth = observed,
-      estimate = predicted,
-      estimator = "macro_weighted"
-  )
-
-  #Step 2 : table
-  table <- table(
-    df[["predicted"]],
-    df[["observed"]]
-  )
-
-  return(list(table,metrics))
 }
+clap <- yardstick::new_class_metric(clap, "maximize")
 
+multi_metric <- yardstick::metric_set(
+  #accuracy,
+  bal_accuracy,
+  #mcc,
+  #clap,
+  precision,
+  recall
+)
