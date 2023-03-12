bb5300719bd4b4502c95f4cb954ceb71eb98ea93
Corrado Lanera <Corrado.Lanera@gmail.com>
2023-03-12 20:28:06 +0100

    .0: Auto stash before merge of "topology-1.0" and "origin/topology-1.0"

--- R/tuning_parameters_tbl.R
+++ R/tuning_parameters_tbl.R
@@ -6,30 +6,46 @@ here::here("runs") |>
     run <- readr::read_rds(.x)
     par <- run[["gg_histories"]][["labels"]][["subtitle"]]
 
-    run[["k_scores"]] |>
-    dplyr::filter(set == "validation") |>
-    dplyr::with_groups(
-      epochs,
-      dplyr::summarise,
-      loss = mean(loss),
-      acc = mean(accuracy)
-    ) |>
-    dplyr::filter(loss == min(loss)) |>
-    dplyr::mutate(
-      rec_units = par |>
-        stringr::str_extract("(?<=Recurrent units: )\\d+"),
-      dense_units = par |>
-        stringr::str_extract("(?<=Dense units: )\\d+"),
-      batch_size = par |>
-        stringr::str_extract("(?<=Batch size: )\\d+"),
-      rec_depth = par |>
-        stringr::str_extract("(?<=Recurrent depth: )\\d+"),
-      crnn_kernel = par |>
-        stringr::str_extract("(?<=ConvLSTM kernel size: )\\d+"),
-      input_do = par |>
-        stringr::str_extract("(?<=Input drop-out: )[\\d.]+"),
-      internal_do = par |>
-        stringr::str_extract("(?<=Internal drop-out: )[\\d.]+")
-    )
-  }, .id = "file") |>
-  dplyr::mutate(file = basename(file))
+    val <- run[["k_scores"]] |>
+      dplyr::filter(set == "validation") |>
+      dplyr::with_groups(
+        epochs,
+        dplyr::summarise,
+        set = unique(set),
+        loss = mean(loss),
+        acc = mean(accuracy)
+      ) |>
+      dplyr::filter(loss == min(loss))
+
+    tr <- run[["k_scores"]] |>
+      dplyr::filter(set == "train") |>
+      dplyr::with_groups(
+        epochs,
+        dplyr::summarise,
+        set = unique(set),
+        loss = mean(loss),
+        acc = mean(accuracy)
+      )
+
+    val |>
+      dplyr::bind_rows(dplyr::filter(tr, epochs == val[["epochs"]])) |>
+      dplyr::mutate(
+        rec_units = par |>
+          stringr::str_extract("(?<=Recurrent units: )\\d+"),
+        dense_units = par |>
+          stringr::str_extract("(?<=Dense units: )\\d+"),
+        batch_size = par |>
+          stringr::str_extract("(?<=Batch size: )\\d+"),
+        rec_depth = par |>
+          stringr::str_extract("(?<=Recurrent depth: )\\d+"),
+        crnn_kernel = par |>
+          stringr::str_extract("(?<=ConvLSTM kernel size: )\\d+"),
+        input_do = par |>
+          stringr::str_extract("(?<=Input drop-out: )[\\d.]+"),
+        internal_do = par |>
+          stringr::str_extract("(?<=Internal drop-out: )[\\d.]+")
+      )
+  },
+  .id = "file") |>
+  dplyr::mutate(file = basename(file)) |>
+  tidyr::pivot_wider(names_from = set, values_from = c(loss, acc))
